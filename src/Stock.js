d863c28cd61050997b5a91a6a90ec08feeb68217
Tweenty_ <tweenty.pro@gmail.com>
2024-02-20 17:10:14 +0100

    add: addProduct method in Stock

--- src/Stock.js
+++ src/Stock.js
@@ -1,3 +1,15 @@
-export class Stock {
+import { Article } from "./Article.js";
 
+export class Stock {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(product) {
+        if (product instanceof Article) {
+            this.products.push(product);
+        } else {
+            throw new Error("Invalid product provided");
+        }
+    }
 }
